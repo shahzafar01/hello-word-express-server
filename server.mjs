ef60ad3226f418403d9ec79d8ba16b1a50eb8782
AhmedRaza <ahmadraza.azt@gmail.com>
2022-09-21 18:55:35 +0500

    express demo

--- server.mjs
+++ server.mjs
@@ -4,7 +4,7 @@ const app = express()
 const port = prcess.env.PORT ||3000
 
 app.get('/', (req, res) => {
-    Console.LOG('Response from Shahmir computer:+ req.ip');
+    Console.LOG('Response from Shahmir computer',+ req.ip);
     res.send('Response from Shahmir computer')
   res.send('Hello World!',+ req.ip);
 })
