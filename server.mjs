f637dba3af9492472f8bca3cbcb836c28547f899
AhmedRaza <ahmadraza.azt@gmail.com>
2022-09-21 18:25:43 +0500

    express server

---
+++ server.mjs
@@ -0,0 +1,14 @@
+import { Console } from 'console'
+import express from 'express'
+const app = express()
+const port = prcess.env.PORT ||3000
+
+app.get('/', (req, res) => {
+    Console.LOG('Response from Shahmir computer:+ req.ip');
+    res.send('Response from Shahmir computer')
+  res.send('Hello World!',+ req.ip);
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
